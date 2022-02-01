03828cdbf738b6363b41c8b21250c5e64ecde0e0
epi-gde <gilles@desve.fr>
2022-02-01 23:53:14 +0100

    wip

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,7 @@
 .onLoad <- function(libname, pkgname) {
   ver <- utils::packageVersion("epiuf")
   datver <- as.character(utils::packageDate("epiuf"))
+  # removed because of CRAN Notice (even if packageStartupMessage is the recommended method )
   # packageStartupMeassage generate a NOTE where it should not...
   # packageStartupMessage("Package epiuf Version",as.character(ver[[1]]),"(",datver,") loaded")
 }
