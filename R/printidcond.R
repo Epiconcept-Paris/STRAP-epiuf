4004946bdfa8717a128b8faf413e568297e4f5fe
epi-gde <gilles@desve.fr>
2022-02-16 10:57:12 +0100

    printIdcond now accept text message and return a list of line of text to print

--- R/printidcond.R
+++ R/printidcond.R
@@ -29,38 +29,62 @@
 
 
 #' PrintIDCond
-#'
+#' prints the list of IDs meeting the specified condition 
+#' data dataset to look in 
+#' threshold = cutoff number for ID reporting, as number
+#' cond = specify variables and outcome to search for eg "Pregnant==1 & Sex==1" or "is.na(Sex)"
+#' text = the specified condition to check for 
+#' PrintIDCond( df,hosp_id2>=38266 & hosp_id2 < 38500,"hosp_id2",threshold = 50)
+
 #' @param data  A dataframe to look for cond  
-#' @param threshold Max number of ID to print 
-#' @param cond  A condition 
-#' @param column Column nameto print
+#' @param cond  A condition to check  
+#' @param text The message to print (if empty the condition is used )
+#' @param threshold cutoff number for ID reporting, as number
+#' @param column Column name of ID to print
 #'
-#' @return nothing but print using cat
+#' @return Message to print as list 
 #' @export 
 #'
 #' @examples
 #' df <- data.frame(Id = 1:4 ,
 #'                     Vaccs = c("pfizer"," ", "pfizer", "moderna"))
-#' PrintIDCond(df,50,Vaccs=="pfizer","Id")
+#' PrintIDCond(df,Vaccs=="pfizer",threshol=30 ,"Id")
 
-PrintIDCond <- function(data, threshold, cond, column="IdData"){
+PrintIDCond <- function(data,  cond, text, threshold , column="id"){
+  
   cond <- substitute(cond)
   if (!typeof(cond)=="language") {cond <- parse(text=cond)}
   if  (!column %in% names(data) ){
     column <- names(data)[1]
   }
+  
   Records <- subset(data,eval(cond),column)
   
+  TextToPrint <- c()
+  
   NbCond <- nrow(Records)
-  if (NbCond > 0 & NbCond < threshold){
-    listID <-   unlist(Records)
-    listID <-  paste(listID, collapse = ", ")
-  }else if(NbCond >= threshold){
-    listID <- paste0(threshold, " or more IDs")
-  }else if(NbCond==0) {
-    listID<- "none"
+  
+  if (NbCond != 0){
+    
+    TextToPrint <- c(TextToPrint,paste0("* ", text,": ", NbCond,"\n"))
+    
+    if (NbCond > 0 & NbCond < threshold){
+      
+      listID <-  unlist(Records)
+      listID <-  paste(listID, collapse = ", ")
+      TextToPrint <- c(TextToPrint, paste0("  + IDs: ", listID, "\n"))
+      
+    }else if(NbCond >= threshold){
+      
+      listID <- paste0(threshold, " or more")
+      TextToPrint <- c(TextToPrint, paste0("  + IDs: ", listID, "\n"))
+      
+    }
+    
   }
-  cat(paste0("+ IDs: ", listID))
+  
+  if (length(TextToPrint) != 0){return(TextToPrint)}
+  
 }
 
 #' Simple count of records satisfying  a conditional expression
