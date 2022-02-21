ccfcf8dc07065c56e7d6ea8ced905b923f09fdfa
epi-gde <gilles@desve.fr>
2022-02-21 13:04:26 +0100

    check added for variables name or date calcultaion, getDealy also work if no condition

--- R/getDelayIf.R
+++ R/getDelayIf.R
@@ -54,10 +54,11 @@ getDelayIf <- function(data, date1, date2, ...) {
   if (length(namedarg) > 0 ) {listcond[namedarg] <- NULL }
   
   # We verify that parameters are language and if it is "test" then we parse as language
-  for (i in 1:length(listcond)) {
-    if (!typeof(listcond[i])=="language") {listcond[i] <- parse(text=listcond[i])}
-  }
-  
+  if (length(listcond)>0) {
+    for (i in 1:length(listcond)) {
+      if (!typeof(listcond[i])=="language") {listcond[i] <- parse(text=listcond[i])}
+    }
+  }  
   # we get the named parameters as "parsed language"
   s_op <- deparse(substitute(date1))
   # if date1 is a variable which contain char, we use content of date1
@@ -80,18 +81,31 @@ getDelayIf <- function(data, date1, date2, ...) {
   )
   date2 <- s_op
   
-  
-  # we add a delay variable to the dataset (should we ?) 
-  data$temp__delay <- as.numeric(data[, date2] - data[, date1])
-  
-  # we loop over condition 
-  for (i in 1:length(listcond)) {
-    onecond = listcond[[1]]
-    # we get index of row which meet the condition 
-    meet <- which(with(data,eval(onecond)))
-    # delay is NA if the condition is not meet
-    data[ ! meet,"temp__delay"] <- NA
+  # check that variables names are ok otherwise stop
+  if (! date1 %in% names(data)) {
+     stop(date1," is not a valid column name")
   }
+  if (! date2 %in% names(data)) {
+    stop(date2," is not a valid column name")
+  }
+
+  # we add a delay variable to the dataset (should we ?) 
+  tryCatch(
+    data$temp__delay <- as.numeric(data[, date2] - data[, date1])
+    , error = function(c) { stop(date1," or ",date2,"  is not a valid date for getDelayIf" )}
+  )
+  
+  
+  # we loop over condition if one exist
+  if (length(listcond)>0) {
+    for (i in 1:length(listcond)) {
+      onecond = listcond[[1]]
+      # we get index of row which meet the condition 
+      meet <- which(with(data,eval(onecond)))
+      # delay is NA if the condition is not meet
+      data[ ! meet,"temp__delay"] <- NA
+    }
+  }  
   result <- data$temp__delay
   data$temp__delay <- NULL
   
