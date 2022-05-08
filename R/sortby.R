ac85fc0c95f21b5a88cd2c759c2595e9aeee8a6d
epi-gde <gilles@desve.fr>
2022-05-08 22:06:33 +0200

    lazzy eval to sortBy

--- R/sortby.R
+++ R/sortby.R
@@ -15,9 +15,18 @@
 #' df <- data.frame(one=c(2,1,2,1),two=c(2,2,1,1))
 #' df <- sortBy(df,"one")
 #' df <- sortBy(df,1) 
-sortBy <- function(data, field, decreasing=FALSE, na.last=TRUE) {
+sortBy <- function(data,field, decreasing=FALSE, na.last=TRUE) {
   data <- as.data.frame(data)
-  vect <- data[,field]
+  sfield <- substitute(field)
+  r <- try(class(field),TRUE)
+  if ( inherits(r, "try-error")) {
+     field <- with(data,sfield) 
+  }  
+  if (is.character(field) & length(field==1)) {
+    vect <- data[,field]
+  } else if (length(field)>1) {
+    vect <-  field
+  } 
   neworder <- order(vect ,na.last=na.last ,decreasing=decreasing )
   data[neworder,]
 }
