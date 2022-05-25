be1e9da1314d2d8fd23d72d8582afeff822c56b3
epi-gde <gilles@desve.fr>
2022-05-25 12:47:28 +0200

    desc and descBy merged and pulled in main

--- R/descBy.R
+++ R/descBy.R
@@ -2,15 +2,15 @@
 # Project Name : STRAP-epiuf
 # Script Name  : descBy
 # GitHub repo  : STRAP-epiuf
-# Summary      : Descriptive table according to another variable
+# Summary      : Descriptive table according or not to another variable
 # Date created : 29/04/2022
 # Author       : MML
 # Date reviewed:
 # Reviewed by  :
 
 # Description --------------------------------------------------------------
-# Function aims to describe variables according to another variable in a table.
-# For example to describe age, sex, bmi by controls/cases status.
+# Function aims to describe variables according or not to another variable in a table.
+# For example to describe age, sex, bmi or to describe age, sex, bmi by controls/cases status.
 # 
 
 
@@ -19,154 +19,246 @@
 
 # START of SCRIPT  --------------------------------------------------------
 
-#' describBy
+describe <- function(data.desc){
+  
+  # Numeric variable
+  if (is.numeric(data.desc))
+  {
+    n <- sum(!is.na(data.desc))
+    na <- sum(is.na(data.desc))
+    na.pct <- round(na/length(data.desc)*100,1)
+    moy <- round(mean(data.desc,na.rm=TRUE),1)
+    sd <- round(sd(data.desc,na.rm=TRUE),1)
+    med <- round(median(data.desc,na.rm=TRUE),1)
+    p25 <- round(quantile(data.desc,c(0.25),na.rm=TRUE),1)
+    p75 <- round(quantile(data.desc,c(0.75),na.rm=TRUE),1)
+    
+    # Variable label
+    l1 <- NA
+    # Mean (SD)
+    l2 <- paste0(moy," (",sd,")")
+    # Median (IQR)
+    l3 <- paste0(med," (",p25,"-",p75,")")
+    # Missing, n(%)
+    if(na == 0)
+    {
+      l4 <- na
+    }else
+    {
+      l4 <- paste0(na," (",na.pct,")")
+    }
+    
+    table.desc <- rbind(l1,l2,l3,l4)
+  }
+  
+  
+  # Categorical variable
+  if (is.factor(data.desc))
+  {
+    # Levels of the variable
+    mod.var <- levels(data.desc)
+    
+    n <- sum(!is.na(data.desc))
+    na <- sum(is.na(data.desc))
+    na.pct <- round(na/length(data.desc)*100,1)
+    n.tab <- as.vector(table(data.desc))
+    n.pct <- round((n.tab/n)*100,1)
+    
+    # Variable label
+    l1 <- NA
+    
+    # For each level j of the variable
+    for(j in 1:length(mod.var))
+    {
+      
+      l2 <- paste0(n.tab[j]," (",n.pct[j],")")
+      l12 <- rbind(l1,l2)
+      l1 <- l12
+    }
+    
+    # n(%) x nb of variable levels (1 line = 1 level)
+    l123 <- l12
+    # Missing, n(%)
+    if(na == 0)
+    {
+      l4 <- na
+    }else
+    {
+      l4 <- paste0(na," (",na.pct,")")
+    }
+    table.desc <- rbind(l123,l4)
+  }
+  return(table.desc)
+}
+
+
+#' descBy
 #'
-#' Aim: to describe numeric variables or categorical variables (must be in factor) according to another variable in a table.
+#' Aim: to describe numeric variables or categorical variables (must be in factor) according or not to another variable in a table.
 #' 
 #' @param data name of the dataset
 #' @param vars the variables list to describe (numeric or factor)
-#' @param by variable to compare (must be a factor and have at least two categories)
-#' @param labels labels for the variables list (var names used if NULL)
+#' @param labels labels for the variables list
+#' @param compVar variable to compare (must be a factor and have at least two categories)
 #' 
 #' @return a descriptive table 
 #' @export
 #'
 #' @examples
-#' \dontrun{
-#'    table <- descBy(data = df, vars = c("age","sex","fluvaccany"), 
+#' \dontrun{ 
+#'    table <- descBy(data, vars = c("age","sex","fluvaccany"), 
+#'    labels = c("Age", "Sex", "Seasonal influenza vaccination"))
+#'    
+#'    table.comp <- descBy(data = df, vars = c("age","sex","fluvaccany"), 
 #'    labels = c("Age", "Sex", "Seasonal influenza vaccination"), 
-#'    by = "lab_flu"))
+#'    compVar = "lab_flu")
 #' }
 #' 
-describBy <- function(data, vars, by, labels=NULL){
-  ok <-  TRUE
-  # Check if compVar is a factor
-  if(class(data[[by]]) != "factor"){
-    warning(by, " is not a factor")
-    ok <-  FALSE
-  }
-  
-  # Levels of compVar
-  mod <- levels(data[[by]])
-  # Check if there are at least 2 categories
-  if(length(mod) < 2){
-    warning(by, " has less than 2 categories")
-    ok <-  FALSE
-  }
-  
-  if (ok == TRUE) {
+descBy <- function(data, vars, labels=NULL, compVar = NULL) {
+  # Build the simple descriptive table
+  data <- as.data.frame(data)
+  if (is.null(labels)) labels <-  vars
+
+    if(is.null(compVar))  {
     # Select list of variables
-    data.comp <- as.data.frame(data[,vars])
-    if (is.null(labels)) labels <-  vars
-    
-  
-    # For each level of compVar retrieve the number of records
-    by <- as.data.frame(data[[by]])
-    mod.nb <- rep(NA, length(mod))
-    for (i in 1:length(mod)){
-      mod.nb[i] <- nrow(subset(data.comp, by==mod[i]))
-    }
-    label.mod <- paste0(mod, "\n(N=", mod.nb, ")")
+    data.subset <-data[,vars]
     
     # Empty object to save the table
-    res <-c(1:(1+length(mod)))
+    table <- NULL
     
-    for(i in 1:length(vars))
+    # Loop in the list of variables to be described
+    for (i in 1:length(vars))
     {
+      var <- data.subset[,i]
+      
       # Numeric variable
-      if (is.numeric(data.comp[,i]))
+      if(is.numeric(var))
       {
         l1 <- labels[i]
         l2 <- "Mean (SD)"
         l3 <- "Median (IQR)"
         l4 <- "Missing, n(%)"
         
-        for(k in 1:length(mod))
-        {
-          data.k <-as.data.frame(data.comp[by==mod[k] & !is.na(by),])
-          n <- sum(!is.na(data.k[,i]))
-          na <- sum(is.na(data.k[,i]))
-          na.pct <- round(na/nrow(data.k)*100,1)
-          moy <- round(mean(data.k[,i],na.rm=TRUE),1)
-          sd <- round(sd(data.k[,i],na.rm=TRUE),1)
-          med <- round(median(data.k[,i],na.rm=TRUE),1)
-          p25 <- round(quantile(data.k[,i],c(0.25),na.rm=TRUE),1)
-          p75 <- round(quantile(data.k[,i],c(0.75),na.rm=TRUE),1)
-          
-          l1 <- c(l1, NA)
-          l2 <- c(l2, paste0(moy," (",sd,")"))
-          l3 <- c(l3, paste0(med," (",p25,"-",p75,")"))
-          l4 <- c(l4, paste0(na," (",na.pct,")"))
-        }
-        
         l1234 <- rbind(l1,l2,l3,l4)
-        
-        table.comp <- rbind(res,l1234)
-        res <-table.comp
       }
       
       # Categorical variable
-      if (is.factor(data.comp[,i]))
+      if (is.factor(var))
       {
-        # Levels of the variable i 
-        mod2 <- levels(data.comp[,i])
+        mod.var <- levels(var)
         
-        # For each category/level j of the variable i
-        for(j in 1:length(mod2))
+        l1 <- labels[i]
+        
+        for(k in 1:length(mod.var))
         {
-          l1 <- c(paste0(labels[i], ", n(%)"), rep(NA, length(mod)))
-          l2 <- mod2[j]
-          
-          for(k in 1:length(mod)){
-            data.k <- data.comp[by==mod[k] & !is.na(by),]
-            n <- sum(!is.na(data.k[,i]))
-            n.tab <- as.vector(table(data.k[,i]))
-            n.pct <- round((n.tab/n)*100,1)
-            
-            if (j==1 & k!=length(mod)) 
-            {
-              l2 <- c(l2,paste(n.tab[j]," (",n.pct[j],")",sep=""))
-            }
-            if (j==1 & k==length(mod))
-            {
-              l2 <- c(l2,paste(n.tab[j]," (",n.pct[j],")",sep=""))
-            }
-            if (j>1 & k!=length(mod))
-            {
-              l1 <- l
-              l2 <- c(l2,paste(n.tab[j]," (",n.pct[j],")",sep=""))
-            }
-            if (j>1 & k==length(mod))
-            {
-              l1 <- l
-              l2 <- c(l2,paste(n.tab[j]," (",n.pct[j],")",sep=""))
-            }            
-          }
-          l <- rbind(l1,l2)
+          l2 <- paste0(mod.var[k],", n(%)")
+          l12 <- rbind(l1,l2)
+          l1 <- l12
         }
         
-        l3 <- "Missing"
+        l4 <- "Missing, n(%)"
+        l1234 <- rbind(l1, l4)
         
-        for(k in 1:length(mod)){
-          data.k <- data.comp[by==mod[k] & !is.na(by),]
-          na <- sum(is.na(data.k[,i]))
-          na.pct <- round(na/nrow(data.k)*100,1)
-          l3 <- c(l3, paste0(na," (",na.pct,")"))
-        }
-        
-        l <- rbind(l,l3)
-        
-        table.comp <- rbind(res,l)
-        res <-table.comp
       }
+      # Merge labels and values
+      res <- rbind(table, cbind(l1234, describe(var)))
+      table <- res
     }
     
-    table.comp <- table.comp[-1,]
-    rownames(table.comp) <- 1:nrow(table.comp)
-    colnames(table.comp) <- c("Variable", label.mod)
-    table.comp <- as.data.frame(table.comp, row.names = 1:length(table.comp))
-    return(table.comp)
-  } else return(NULL)  
+    colnames(table) <- c("Variables", "Description")
+    table <- as.data.frame(table, row.names = 1:nrow(table))
+    return(table)
+    
+  }else{
+    
+    # Select list of variables
+    data.subset <- data[,c(vars,compVar)]
+    
+    # Check if compVar is a factor
+    if(class(data[,compVar]) != "factor")
+    {
+      stop(compVar, " is not a factor")
+    }
+    
+    # Levels of compVar
+    mod.compVar <- levels(data[,compVar])
+    # Check if there are at least 2 categories
+    if(length(mod.compVar) < 2)
+    {
+      stop(compVar, " has less than 2 categories")
+    }
+    
+    # For each level of compVar retrieve the number of records
+    mod.compVar.nb <- rep(NA, length(mod.compVar))
+    for (i in 1:length(mod.compVar))
+    {
+      mod.compVar.nb[i] <- nrow(data.subset[data.subset[[compVar]] == mod.compVar[i],])
+    }
+    mod.CompVar.label <- paste0(mod.compVar, "\n(N=", mod.compVar.nb, ")")
+    
+    # Empty object to save the table
+    table <- NULL
+    
+    # Loop in the list of variables to be described
+    for (i in 1:length(vars))
+    {
+      var <- data.subset[,i]
+      
+      # Numeric variable
+      if(is.numeric(var))
+      {
+        l1 <- labels[i]
+        l2 <- "Mean (SD)"
+        l3 <- "Median (IQR)"
+        l4 <- "Missing, n(%)"
+        
+        l1234 <- rbind(l1,l2,l3,l4)
+        
+        all.res <- NULL
+        for(k in 1:length(mod.compVar))
+        {
+          data.k <- data.subset[data.subset[[compVar]] == mod.compVar[k],]
+          res <- describe(data.k[,vars[i]])
+          all.res <- cbind(all.res, res)
+        }
+      }
+      
+      # Categorical variable
+      if (is.factor(var))
+      {
+        mod.var <- levels(var)
+        
+        l1 <- labels[i]
+        
+        for(k in 1:length(mod.var))
+        {
+          l2 <- paste0(mod.var[k],", n(%)")
+          l12 <- rbind(l1,l2)
+          l1 <- l12
+        }
+        
+        l4 <- "Missing, n(%)"
+        l1234 <- rbind(l1, l4)
+        
+        all.res <- NULL
+        for(k in 1:length(mod.compVar))
+        {
+          data.k <- data.subset[data.subset[[compVar]] == mod.compVar[k],]
+          res <- describe(data.k[,vars[i]])
+          all.res <- cbind(all.res, res)
+        }
+      }
+      
+      # Merge labels and values
+      res <- rbind(table, cbind(l1234, all.res))
+      table <- res
+    }
+    
+    rownames(table) <- 1:nrow(table)
+    colnames(table) <- c("Variable", mod.CompVar.label)
+    table <- as.data.frame(table)
+    return(table)
+  }
 }
 
 
