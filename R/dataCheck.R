f5df6aa8ba3904b4102c6714b5a2e1516bcb13f6
epi-jhd <j.howard@epiconcept.fr>
2022-06-07 15:23:05 +0200

    WIP data check functions. See datacheck.rmd in vignette for summary and issues.

--- R/dataCheck.R
+++ R/dataCheck.R
@@ -49,8 +49,8 @@ countIf <- function(data,cond=NULL) {
     
     # subset will make a lazy eval, in order to work, this one should be done in countIf context 
     # then parent.frame(2)
-     #records <- subset(data,eval(cond,data,parent.frame(2)))
-    records <- data[eval(quote(cond)),]
+    records <- subset(data,eval(cond,data,parent.frame(2)))
+    #records <- data[eval(quote(cond)),]
   } else records <- data  
   r <- nrow(records)
   return(r)
@@ -80,7 +80,7 @@ countIf <- function(data,cond=NULL) {
 #' listIf(data=df1, varname="Id", cond = A=="b", na.rm=TRUE)
 #' 
 
-listIf <- function(data, varname=NULL, cond=NULL, collapse=FALSE,na.rm = FALSE){
+listIf <- function(data, varname=NULL, cond=NULL,  collapse=FALSE, na.rm = FALSE, data_old=NULL){
 
   # set up 2 warnings
   warning1 <- NULL
@@ -97,33 +97,62 @@ listIf <- function(data, varname=NULL, cond=NULL, collapse=FALSE,na.rm = FALSE){
     warning1 <- paste0(varname, " is not in dataset, first column used")
     varname <- names(data)[1]
   }
-  
-  
+ 
+  # enable cond to be used in subset function
   cond <- substitute(cond)
   
-  if (!is.null(cond)) {
+  if (!is.null(cond)) { # if cond has been given
     if (!typeof(cond)=="language") {cond <- parse(text=cond)}
 
   # Retrieve records matching cond
     # subset will make a lazy eval, in order to work, this one should be done in listIf context 
     # then parent.frame(2)
-    records <- subset(data,eval(cond,data,parent.frame(2)))
+    Records <- subset(data,eval(cond,data,parent.frame(2)), varname)
   } else {
-    records <- data
+    Records <- data[,varname]
     warning2 <- "No condition provided, all records listed"
   }
 
+  if (isTRUE(na.rm)){
+    Records <- Records[!is.na(Records[,varname]),varname]
+  }
   
-    if (isTRUE(na.rm)){
-      listID <- records[!is.na(records[,varname]),varname]
-    }else{
-      listID <- records[,varname]
+  listID <- Records
+  
+  if(collapse==TRUE){ # if defined, collapse to string output
+    listID <- paste(listID, collapse= ", ")  
+  }
+  
+    if(!is.null(data_old)){ # if a second dataset is provided
+
+      if (!is.null(cond)) { # if cond has been given
+        if (!typeof(cond)=="language") {cond <- parse(text=cond)}
+        # Retrieve records matching cond
+        # subset will make a lazy eval, in order to work, this one should be done in listIf context 
+        # then parent.frame(2)        
+    Records_old <- subset(data_old,eval(cond,data,parent.frame(2)),varname) # get records list from old set
+      } else {
+        Records_old <- data_old[,varname]
+      }
+      
+    if (isTRUE(na.rm)){ # remove NA if na.rm is TRUE
+      Records_old <- Records_old[!is.na(Records_old[,varname]),varname]
     }
-  
-    if (collapse==TRUE){
-      listID <- paste(listID, collapse= ", ")
+    
+    listIDold <- intersect(Records[, varname],Records_old[,varname]) # list those in new that are also in old (ie are repeats)
+    listIDnew <- setdiff(Records[, varname], listIDold) # list those only in the new
+    
+    listID <- list(repeats = listIDold, new = listIDnew)
+    
+    if(collapse==TRUE){
+      listIDold <-  paste(listIDold, collapse = ", ") # collapse repeat IDs
+      
+      listIDnew <-  paste(listIDnew, collapse = ", ") # collapse new IDs
+      
+      listID <- paste0("[ ", listIDold, " ]  ", listIDnew) # combine to list IDs
     }
-  
+    }
+
   if(!is.null(warning1)&is.null(warning2)){warning(warning1)}
   if(is.null(warning1)&!is.null(warning2)){warning(warning2)}
   if(!is.null(warning1)&!is.null(warning2)){warning(paste0(warning1, "\n", warning2))}
@@ -133,8 +162,9 @@ listIf <- function(data, varname=NULL, cond=NULL, collapse=FALSE,na.rm = FALSE){
 
 
 #' printIf2
-#' prints the list of IDs meeting the specified condition
-#'  
+#' Prints the caption, count and list of IDs meeting the specified condition in 
+#' rmd bullet point format. Aim is to replace printIf.
+#' 
 #' @param data  A dataframe to look for condition  
 #' @param cond  A condition to check  
 #' @param text The message to print (if empty the condition is used)
@@ -161,39 +191,80 @@ printIf2<- function(data,  cond, text = "", threshold = NULL , varname = "id", n
   TextToPrint <- c()
   
 # retrieve count and list  
-  NbCond <- countIf(data=data, cond=cond)
- Records <- listIf(data=data, varname=varname, cond=cond, na.rm=na.rm)
+ NbCond <- countIf(data=data, cond=cond)
  
 # if count not 0, output in desired format 
   if (NbCond != 0){
     if (NbCond > 0 & NbCond < threshold){ #if number is under threshold
-      
-      if(!is.null(data_old)){# if a second dataset is provided
-        
-        Records_old <- listIf(data_old,varname=varname, cond=cond, na.rm=na.rm) # get records list from old set
-        
-        inOld <- intersect(Records,Records_old) # list those in new that are also in old (ie are repeats)
-        inNew <- setdiff(Records, inOld) # list those only in the new
-        
-        listIDold <-  paste(inOld, collapse = ", ") # collapse repeat IDs
-        listIDnew <-  paste(inNew, collapse = ", ") # collapse new IDs
-        
-        listID <- paste0("[ ", listIDold, " ]  ", listIDnew) # combine to list IDs
-        
-      } else {# if no second dataset provided, output ids 
-        listID <- paste(Records, collapse= ", ")
-      }
+      listID <- listIf(data=data, varname=varname, cond=cond, collapse=TRUE, na.rm=na.rm, data_old=data_old)
     }else if (NbCond >= threshold){
       listID <- paste0(threshold, " or more")
     }
     TextToPrint <- c(paste0("* ", text,": ", NbCond,"\n","  + IDs: ", listID, "\n"))
-    
   }
   
   
   if (length(TextToPrint) != 0){return(TextToPrint)}
   
 }
+
+
+#' printKableIf
+#' Takes any number of lists of caption-condition pairs, and vector of headers to match each list.
+#' Outputs a table sectioned / list with header.
+#' 
+#' @param data 
+#' @param parameters 
+#' @param header 
+#' @param threshold 
+#' @param varname 
+#' @param na.rm 
+#' @param data_old 
+#'
+#' @return table
+#' @export
+#'
+#' @examples
+#' parameter <- list(
+#' "!is.na(SariWho) & SariWho==0 & ((!is.na(SympFever)&SympFever==0) | (!is.na(SympCough) & SympCough==0))" = "**Not meeting SARI case definition due to not having fever or cough symptoms**"
+#', "!is.na(SariWho)&SariWho==0&is.na(Dischargedate)&!is.na(Outcome)&Outcome==2" = "**Not meeting SARI case definition due to missing hospital discharge date when outcome is discharged**"
+#', "!is.na(SariWho)&SariWho==0&is.na(Deathdate)&!is.na(Outcome)&Outcome==1" = "**Not meeting SARI case definition due to missing hospital death date when outcome is deceased**"
+#', "!is.na(SariWho)&SariWho==0&is.na(Transferdate)&!is.na(Outcome)&Outcome==4" = "**Not meeting SARI case definition due to missing hospital transfer date when outcome is transferred.**"
+#')
+printKableIf <- function(data,  parameters, header="Data Check", threshold = NULL , varname = "id", na.rm = FALSE, data_old=NULL){
+   
+  #If not threshold is provided, default threshold is 50
+  if (is.null(threshold)) {threshold <- 50 } 
+
+  datacheck <- data.frame("Check"="", "Number"="", "Id" = "")#create empty dataframe 
+  
+  for (i in 1:length(parameters)){# loop through all condition text pairs in the list
+    
+    cond <- parse(text=names(parameters[i])) # retrieve condition from list
+    text <-as.character(parameters[i]) # retrieve text caption from list
+    
+    NbCond <- countIf(data=data, cond=cond)
+    listID <- NULL
+    
+    if (NbCond != 0){
+      if (NbCond > 0 & NbCond < threshold){ #if number is under threshold
+        listID <- listIf(data=data, varname=varname, cond=cond, collapse=TRUE, na.rm=na.rm, data_old=data_old)
+      }else if (NbCond >= threshold){
+        listID <- paste0(threshold, " or more")
+      }
+    if (!is.null(listID)){
+      RowToPrint <- c(text, NbCond,listID) # assemble row for each check
+      datacheck <- rbind(datacheck, RowToPrint) # append to data frame
+    }
+    }
+  }
+  
+  if (nrow(datacheck[-1,]) != 0){# if dataframe has any rows
+    return(kable(datacheck[-1,],format = "pipe", row.names=F, padding=50
+                 , align = "lll", caption=paste0(header, " :")))# output table with header as caption
+  }
+  }
+
 
 #' dataCheck
 #'
