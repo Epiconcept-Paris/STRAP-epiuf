f125bd5467e8831fcde9f7809f05f9dbc422d299
loremerdrignac <l.merdrignac@epiconcept.fr>
2022-12-20 17:16:01 +0100

    Suggestion to Xlsx functions, adding style conditions to fillCell

--- R/xlsxTools.R
+++ R/xlsxTools.R
@@ -7,7 +7,7 @@
 
 
 
-# epixlsx envirronement used to manage global values
+# epixlsx environment used to manage global values
 epixlsx_env <- new.env(parent = emptyenv())
 
 epixlsx_env$report <- NULL
@@ -23,7 +23,7 @@ epixlsx_env$reportFilename <- NULL
 #' getWorkbook()
 #' 
 getWorkbook <- function() {
-  return(  epixlsx_env$report )
+  return(epixlsx_env$report)
 }
 
 #' fillCells
@@ -37,50 +37,207 @@ getWorkbook <- function() {
 #' @param colnames if TRUE column names of the dataframe are displayed 
 #' @param rownames if TRUE row names of the dataframe are displayed             
 #' @param style An optional style created with createXlsxStyle   
+#' @param styleRowsIndex Numeric vector of the rown index where to apply the style
+#' @param styleColsIndex Numeric vector of the column index where to apply the style
 #' @param wb An optional wb if not already opened   
 #' @return  nothing
 #' @export
-#' @importFrom openxlsx  writeData col2int addWorksheet
+#' @importFrom openxlsx  writeData col2int 
 #'
 #' @examples
+#' 
+#' # Creating an empty workbook
 #' wb <- openxlsx::createWorkbook()
 #' sheetname <- "First Sheet"
 #' openxlsx::addWorksheet(wb, sheetName = sheetname)
 #' num <- 3
 #' denum <- 10
-#' fillCells(sheetname,1,1, num,denum , wb=wb )
+#' fillCells(sheetname, 1, 1, num, denum , wb = wb)
 #' mat <- data.frame(Id = 1:3 , Vaccs = c("1", "3", "6"))
-#' fillCells(sheetname,1,1, mat ,wb=wb )
+#' fillCells(sheetname, 3, 1, mat, wb = wb)
 #' 
-fillCells <- function(onesheet,line,col, ... , names = FALSE, colnames=FALSE, 
-                      rownames=FALSE, style = NULL,  wb = NULL) {
+#' # Writing a data frame in the workbook using a style to apply for a set of rows and columns
+#' alertStyle <- createXlsxStyle(textDecoration = "Bold", fontColour = "red")
+#' fillCells(wb = wb, 
+#'           onesheet = sheetname, 
+#'           line = 7, 
+#'           col = 2, 
+#'           iris, 
+#'           style = alertStyle, 
+#'           styleRowsIndex = which(iris$Sepal.Length<5),
+#'           styleColsIndex = 1:4)
+#'           
+#' # Saving the Excelfile
+#' \dontrun{
+#' saveXlsx(filename = "test.xlsx", wb = wb)
+#' }
+#'
+fillCells <- function(onesheet,
+                      line,
+                      col, 
+                      ... , # All following arguments are optional but should be named 
+                      names = FALSE, 
+                      colnames = FALSE, 
+                      rownames = FALSE, 
+                      style = NULL, 
+                      styleRowsIndex = NULL,  
+                      styleColsIndex = NULL,
+                      wb = NULL) {
   
-  # If col is provided as a character string (i.e., col = "A") then converted as column index 
+  # If col is provided as a character string (i.e., col = "A") 
+  # then will be converted as column index 
   if (is.character(col)){
     col <- openxlsx::col2int(col)
   }
   
+  # Default workbook being in package's env
+  if (is.null(wb)) wb <- epixlsx_env$report #LM: why is it in the loop and not outside the loop?
+  
+  # Global argument 'names' setting both colnames and rownames 
   if (names == TRUE) {
     colnames = TRUE
     rownames = TRUE 
   }
   
-   listval <- list( ...)
-   for (i in 1:length(listval)) {
-     value <- ...elt(i)   
-     if (is.numeric(value)) {
-        value <- ifelse(is.finite(value),value,"")
-     }
-     if (is.null(wb)) wb <- epixlsx_env$report # LM: why is it in the loop and not outside the loop?
-     openxlsx::writeData( wb , onesheet, x = value, startCol = col, startRow = line,
-               colNames = colnames, rowNames = rownames)
-     if (! is.null(style)) {
-        openxlsx::addStyle(wb,onesheet,style,line,col)
-     }   
-     col <- col +1
+  # Warning the end user if style is missing
+  if(is.null(style) & (!is.null(styleRowsIndex) | !is.null(styleColsIndex))) {
+    warning("'styleRowsIndex'/'styleColsIndex' are provided but 'style' argument is missing. No style willbe applied")
+  }
+  
+  # Fetching all objects to print in xlsx
+  listval <- list( ...) 
+  for (i in 1:length(listval)) { 
+    value <- ...elt(i)   
+    
+    # Making sure any numeric vector is finite or replace with empty string
+    if (is.numeric(value)) {
+      value <- ifelse(is.finite(value), value, "")
+    }
+    
+    # Write in workbook
+    openxlsx::writeData(wb = wb, 
+                        sheet = onesheet, 
+                        x = value, 
+                        startCol = col, 
+                        startRow = line,
+                        colNames = colnames, 
+                        rowNames = rownames)
+    
+    # Adding style if required
+    if (!is.null(style)) {
       
+      # Checking if condition to apply the style is valid or not
+      if(!is.null(styleRowsIndex) | !is.null(styleColsIndex)) { 
+        
+        # For list or array
+        
+        if(inherits(value, c("list", "array"))) {
+          warning("Class ", class(value), " is currently not supported for style in fillCell")
+        } else if(inherits(value, "data.frame")) {
+          
+          # For data frames
+          
+          # Checking that style condition is TRUE/FALSE
+          if (!(typeof(styleRowsIndex) %in% c("integer", "double", "NULL")) | 
+              !(typeof(styleColsIndex) %in% c("integer", "double", "NULL"))) {
+            warning("Type not supported for 'styleRowsIndex' / 'styleColsIndex' arguments")
+          }
+          
+          # Checking if one of the condition is NULL and replace it with dimension of the object
+          if(is.null(styleRowsIndex)) {styleRowsIndex <- 1:nrow(value)}
+          if(is.null(styleColsIndex)) {styleColsIndex <- 1:ncol(value)}
+            
+          # Adding style at the proper location
+          openxlsx::addStyle(wb = wb, 
+                             sheet = onesheet, 
+                             style = style, 
+                             rows = line + styleRowsIndex - 1, 
+                             cols = col + styleColsIndex - 1,
+                             gridExpand = TRUE)
+          
+        } else {
+          
+          # For atomic variables or vector
+          
+          # Checking that style condition is TRUE/FALSE
+          if (!(typeof(styleRowsIndex) %in% c("integer", "double"))) {
+            warning("Type not supported for 'styleRowsIndex' / 'styleColsIndex' arguments")
+          }
+          
+          # Adding style at the proper location
+          openxlsx::addStyle(wb = wb, 
+                             sheet = onesheet, 
+                             style = style, 
+                             rows = line + styleRowsIndex - 1, 
+                             cols = col)
+        }  # end List, Array, Data frame, Vector, Variable 
+        
+      } else { # If styleIndex is NULL, then style is expanded to the size of the object
+        
+        # List or array
+        
+        if(inherits(value, c("list", "array"))) {
+          warning("Class ", class(value), " is currently not supported for style in fillCell")
+        } else if(inherits(value, "data.frame")) {
+          
+          # Data frames
+          openxlsx::addStyle(wb = wb, 
+                             sheet = onesheet, 
+                             style = style, 
+                             rows = line + 1:nrow(value) - 1, 
+                             cols = col + 1:ncol(value) - 1,
+                             gridExpand = TRUE)
+          
+        } else {
+          
+          # Vectors or variables
+          openxlsx::addStyle(wb = wb, 
+                             sheet = onesheet, 
+                             style = style, 
+                             rows = line + 1:length(value) - 1, 
+                             cols = col,
+                             gridExpand = TRUE)
+          
+        } # end List, Array, Data frame, Vector, Variable
+      } # end no styleIndex
+    } # end Style
+    
+    col <- col +1
+    
   }  # end loop i
 }
+# # Previous version below
+# fillCells <- function(onesheet,line,col, ... , names = FALSE, colnames=FALSE, 
+#                       rownames=FALSE, style = NULL, stylecondition = NULL,  wb = NULL) {
+#   
+#   # If col is provided as a character string (i.e., col = "A") then converted as column index 
+#   if (is.character(col)){
+#     col <- openxlsx::col2int(col)
+#   }
+#   
+#   if (names == TRUE) {
+#     colnames = TRUE
+#     rownames = TRUE 
+#   }
+#   
+#    listval <- list( ...)
+#    for (i in 1:length(listval)) {
+#      value <- ...elt(i)   
+#      if (is.numeric(value)) {
+#         value <- ifelse(is.finite(value),value,"")
+#      }
+#      if (is.null(wb)) wb <- epixlsx_env$report # LM: why is it in the loop and not outside the loop?
+#      openxlsx::writeData(wb, onesheet, x = value, startCol = col, startRow = line,
+#                colNames = colnames, rowNames = rownames)
+#      
+#      # Adding style if required
+#      if (!is.null(style)) {
+#          openxlsx::addStyle(wb, onesheet, style, line, col)
+#      }   
+#      col <- col +1
+#       
+#   }  # end loop i
+# }
 
 
 #' formatCells
@@ -102,7 +259,7 @@ formatCells <- function(onesheet, line, col , style = NULL, wb = NULL)
   }
   if (is.null(wb)) wb <- epixlsx_env$report
     if (! is.null(style)) {
-    openxlsx::addStyle(wb,onesheet,style,line,col) #LM: could we add ... in order to possibly benefit from additional arguments?
+    openxlsx::addStyle(wb, onesheet, style, line, col) # LM: could we add ... in order to possibly benefit from additional arguments like createStyle?
   }   
   
 }  
@@ -163,8 +320,8 @@ fillimage <- function(onesheet,image,line,col, wide=7, high=4 , unit = "in", spe
 #' @param wb       A optional workbook created or opened by xlsx.
 #'                 If not specified, the last workbook loaded with openXlsx will be used
 #'
-#' @return  A cells object containing an Excel sheet
-#' @importFrom openxlsx createWorkbook addWorksheet   
+#' @return  Character string of the name of the sheet
+#' @importFrom openxlsx createWorkbook    
 #' @export
 #'
 #' @examples
@@ -182,7 +339,7 @@ openSheet <- function(sheetname, wb = NULL)  {
     } 
   }
   
-  # sheets <- openxlsx::sheets(report)  # names is not exported ??
+  # sheets <- openxlsx::sheets(report)  # GD: names is not exported ??
   sheets <- names(report) # LM: Replaced openxlsx::sheets with base::names as this function is deprecated  
   if ( !(sheetname %in% sheets)) {
      warning("Sheet ", sheetname, " doesn't exist in workbook")
@@ -225,7 +382,22 @@ openXlsx <- function(filename="") {
 #'
 #' @examples
 #' 
-#' cat("to be done")
+#' # Creating an empty workbook
+#' wb <- openxlsx::createWorkbook()
+#' sheetname <- "First Sheet"
+#' openxlsx::addWorksheet(wb, sheetName = sheetname)
+#' 
+#' # Writing a data frame in the workbook 
+#' fillCells(wb = wb, 
+#'           onesheet = sheetname, 
+#'           line = 1, 
+#'           col = 1, 
+#'           iris)
+#'           
+#' # Saving the Excel file
+#' \dontrun{
+#' saveXlsx(filename = "test.xlsx", wb = wb)
+#' }
 #' 
 saveXlsx <- function(filename="",wb=NULL)  {
   if (! is.null(wb)) {
