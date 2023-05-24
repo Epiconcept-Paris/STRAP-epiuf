dd573bdb18f703849e52bebe6d8246d5fbc84076
epi-clz <c.lopez@epiconcept.fr>
2023-05-24 16:51:01 +0200

    utilsMath script created with getMax and getMin

--- R/utilsMath.R
+++ R/utilsMath.R
@@ -10,9 +10,7 @@
 
 # Description --------------------------------------------------------------
 # 
-# 
-# 
-# 
+# Set of maths functions used widely
 # 
 
 
@@ -20,3 +18,44 @@
 # 
 
 # START of SCRIPT  --------------------------------------------------------
+
+#' getMax
+#' 
+#' get the maximum value from a vector skipping NA values. If all values are NA returns NA.
+#' 
+#' @param x vector
+#' @returns numeric
+#' @export
+#' @author STRAP team \email{strap@epiconcept.fr}
+#' @seealso
+#' For more details see the link below to access the vignette:
+#' \href{../doc/epiuf_package.html}{\code{vignette("epiuf_package")}}
+#'
+#' @examples
+#' getMax(c(1,1))
+#' getMax(c(NA,NA))
+getMax <- function(x){
+  # if all=NA, returns NA
+  if (!all(is.na(x))) max(x, na.rm = TRUE) else NA
+} 
+
+#' getMin
+#' 
+#' get the minimum value from a vector skipping NA values. If all values are NA returns NA.
+#' 
+#' @param x vector
+#' @returns numeric
+#' @export
+#' @author STRAP team \email{strap@epiconcept.fr}
+#' @seealso
+#' For more details see the link below to access the vignette:
+#' \href{../doc/epiuf_package.html}{\code{vignette("epiuf_package")}}
+#'
+#' @examples
+#' getMin(c(1,1))
+#' getMin(c(NA,NA))
+getMin <- function(x) {
+  # if all=NA, returns NA
+  if (!all(is.na(x))) min(x, na.rm = TRUE) else NA
+} 
+
