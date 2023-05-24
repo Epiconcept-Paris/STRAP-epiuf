c058a00015318d07458ac4d237d02326198d8dc4
epi-clz <c.lopez@epiconcept.fr>
2023-05-24 16:20:39 +0200

    new script to place the math functions

---
+++ R/utilsMath.R
@@ -0,0 +1,22 @@
+#
+# Project Name : STRAP Project
+# Script Name  : utilsMath.R
+# GitHub repo  : 
+# Summary      : 
+# Date created : 
+# Author       : 
+# Date reviewed:
+# Reviewed by  :
+
+# Description --------------------------------------------------------------
+# 
+# 
+# 
+# 
+# 
+
+
+# Changes Log --------------------------------------------------------------
+# 
+
+# START of SCRIPT  --------------------------------------------------------
