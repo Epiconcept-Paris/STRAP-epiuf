350c7ea2179c295b249bcb8ffae8ebc74c5b9935
epi-clz <c.lopez@epiconcept.fr>
2023-09-28 19:41:39 +0800

    change red for warning

--- R/compareDataSet.R
+++ R/compareDataSet.R
@@ -95,9 +95,7 @@ updateDataset <- function(data, modele) {
       data[data.add[i]] <- NA
     }
     ## PR_CLZ: Add a message warning the user about the columns created
-    catret("")
-    red(paste0("Column(s) ",paste0("'",data.add,"'",collapse = ","), " created automatically as blank in dictionary sheet"))
-    catret("\n")
+    warning("Column(s) ",paste0("'",data.add,"'",collapse = ","), " created automatically as blank in dictionary sheet\n")
   }
   return(data)
 }
