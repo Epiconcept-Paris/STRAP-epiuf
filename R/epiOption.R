ca523dfba875cec88593fcc81f89b63caf438af8
epi-gde <gilles@desve.fr>
2023-11-04 00:06:55 +0100

    elapsedTime added to mesure processing time

--- R/epiOption.R
+++ R/epiOption.R
@@ -46,6 +46,26 @@ epiutils_env$stat_digits <- 2
 # The current selection applied to the current dataframe
 epiutils_env$select <- ""
 
+epiutils_env$timer <- Sys.time()
+
+
+#' elapsedTime
+#'
+#' @param set TRUE to start 
+#'
+#' @return elapsed time
+#' @export
+#'
+#' @examples
+#' elapsedTime(TRUE)
+elapsedTime <- function(set = FALSE) {
+  if(set)
+    epiutils_env$timer <- Sys.time()
+  else
+    (Sys.time() - epiutils_env$timer)
+}
+
+
 #' getEpiOption
 #'
 #' retrieve an epiutils package option or parameter
